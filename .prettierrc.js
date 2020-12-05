6e9a64acaea1a4092494390725f940cf7db24c20
Hu Jindi <hujindi1212@gmail.com>
2020-12-05 16:10:34 +0800

    define api endpoints in config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	jsxSingleQoute: false,
+	singleQuote: true,
+	trailingComma: 'none',
+	printWidth: 120,
+	useTabs: true,
+	tabWidth: 4,
+	arrowParens: 'always'
+};
---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+	apiEndpoints: {
+		trafficImages: 'https://api.data.gov.sg/v1/transport/traffic-images',
+		weatherForecast: {
+			twoHour: 'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast',
+			twentyFourHour: 'https://api.data.gov.sg/v1/environment/24-hour-weather-forecast',
+			fourDay: 'https://api.data.gov.sg/v1/environment/4-day-weather-forecast'
+		}
+	}
+};
+
+export default config;
